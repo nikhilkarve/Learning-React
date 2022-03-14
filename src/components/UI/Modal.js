ed91d35616c820045720076917caacc92b924a83
Nikhil Karve <nikhilkarve@Nikhils-MacBook-Air.local>
2022-03-14 14:34:03 -0400

    Completed v1

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import { Fragment } from "react/cjs/react.production.min";
 import ReactDom from "react-dom";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClick}></div>;
 };
 
 const ModalOverlay = (props) => {
@@ -25,7 +25,10 @@ const Modal = (props) => {
     // </Fragment>
     // Now with portal
     <Fragment>
-      {ReactDom.createPortal(<Backdrop />, portalElement)}
+      {ReactDom.createPortal(
+        <Backdrop onClick={props.onClick} />,
+        portalElement
+      )}
       {ReactDom.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
